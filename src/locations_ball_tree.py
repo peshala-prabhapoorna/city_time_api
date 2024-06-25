0f31a46f7a858807d9529f298ad3ebc6bf4063ff
peshala-prabhapoorna <peshalaprabhapoorna@gmail.com>
2024-06-25 11:18:32 +0530

    reformat code with ruff

--- src/locations_ball_tree.py
+++ src/locations_ball_tree.py
@@ -2,26 +2,31 @@ from sklearn.neighbors import BallTree
 from pydantic import BaseModel
 import numpy as np
 
+
 class Location(BaseModel):
     latitude: float
     longitude: float
 
+
 def ball_tree(db_cursor):
-    # get latitudes, latitudes from database 
+    # get latitudes, longitude from database
     query = "SELECT latitude, longitude FROM city_data;"
     query_result = db_cursor.execute(query)
     locations = query_result.fetchall()
 
     # convert degress to radian and locations to 2d list
-    locations = [[np.deg2rad(latitude), np.deg2rad(longitude)] 
-                 for (latitude, longitude) in locations]
+    locations = [
+        [np.deg2rad(latitude), np.deg2rad(longitude)]
+        for (latitude, longitude) in locations
+    ]
     # convert locations to a numpy array
     locations = np.array(locations)
 
     # create ball tree of locations
     bt = BallTree(locations, metric="haversine")
 
-    return bt 
+    return bt
+
 
 def query_ball_tree(location, ball_tree):
     query_location = [location["latitude"], location["longitude"]]
--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from datetime import datetime
 import sqlite3
-import json
 
 from locations_ball_tree import ball_tree, query_ball_tree, Location
 from utilities import LocationEntry
@@ -15,62 +14,72 @@ db_cursor = db_connection.cursor()
 # create ball tree from city latitudes and longitidues
 city_ball_tree = ball_tree(db_cursor)
 
+
 @app.get("/")
 async def root():
     utc_time = datetime.utcnow()
 
-    return {"time_format": "utc",
-            "hours": utc_time.hour,
-            "minutes": utc_time.minute,
-            "seconds": utc_time.second}
+    return {
+        "time_format": "utc",
+        "hours": utc_time.hour,
+        "minutes": utc_time.minute,
+        "seconds": utc_time.second,
+    }
+
 
 @app.get("/tz/{city}")
 async def city_time(city: str):
     query = "SELECT country, city, timezone, utcoffset \
-        FROM city_data \
-        WHERE city=?;"
+            FROM city_data \
+            WHERE city=?;"
     query_result = db_cursor.execute(query, (city,))
     city_data = query_result.fetchone()
 
-    return {"country": city_data[0],
-            "city": city_data[1],
-            "timezone": city_data[2],
-            "utcoffset": city_data[3]}
+    return {
+        "country": city_data[0],
+        "city": city_data[1],
+        "timezone": city_data[2],
+        "utcoffset": city_data[3],
+    }
+
 
 @app.post("/city/")
 async def nearest_city(location: Location):
     location = location.dict()
     distance, db_id = query_ball_tree(location, city_ball_tree)
-    
+
     # query database to find the city
     query = "SELECT country, city, timezone, utcoffset \
-        FROM city_data \
-        WHERE id=?;"
+            FROM city_data \
+            WHERE id=?;"
     query_result = db_cursor.execute(query, (int(db_id),))
     city = query_result.fetchone()
-    
+
     EARTH_RADIUS = 6371
     distance *= EARTH_RADIUS
-    
-    return {"country": city[0],
-            "city": city[1],
-            "timezone": city[2],
-            "utcoffset": city[3],
-            "distance": distance}
+
+    return {
+        "country": city[0],
+        "city": city[1],
+        "timezone": city[2],
+        "utcoffset": city[3],
+        "distance": distance,
+    }
+
 
 @app.get("/gap/")
 async def time_difference(city_1: str, city_2: str):
     # query database to find cities
     query = "SELECT country, city, timezone, utcoffset \
-       FROM city_data \
-       WHERE city IN (?, ?);"
+            FROM city_data \
+            WHERE city IN (?, ?);"
     query_result = db_cursor.execute(query, (city_1, city_2))
     cities = query_result.fetchall()
 
     # if either of the cities are not found return None
-    if len(cities) !=2:
+    if len(cities) != 2:
         return None
-    
+
     # calculate time gap
     gap = 0
     if cities[0][1] == city_1:
@@ -78,28 +87,34 @@ async def time_difference(city_1: str, city_2: str):
     else:
         gap = cities[0][3] - cities[1][3]
 
-    return {"city_1": city_1,
-            "city_2": city_2,
-            "gap": gap}
+    return {"city_1": city_1, "city_2": city_2, "gap": gap}
+
 
 @app.post("/add/")
 async def add_location(location: LocationEntry):
     location = location.dict()
     # insert location to table
     entry = "INSERT INTO added_locations \
-                (country, city, name, latitude, longitude, description) \
+            (country, city, name, latitude, longitude, description) \
             VALUES (?, ?, ?, ?, ?, ?);"
-    values = (location["country"], location["city"], location["name"],
-                location["latitude"], location["longitude"], 
-                location["description"])
+    values = (
+        location["country"],
+        location["city"],
+        location["name"],
+        location["latitude"],
+        location["longitude"],
+        location["description"],
+    )
     db_cursor.execute(entry, values)
     db_connection.commit()
-    
+
     # query the database to check the entry
     query = "SELECT * FROM added_locations WHERE name = ?;"
     query_result = db_cursor.execute(query, (location["name"],))
     added_location = query_result.fetchone()
 
-    return {"country": added_location[1],
-            "city": added_location[2],
-            "name": added_location[3]}
+    return {
+        "country": added_location[1],
+        "city": added_location[2],
+        "name": added_location[3],
+    }
